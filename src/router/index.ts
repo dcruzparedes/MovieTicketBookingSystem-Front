dc9b98ded605cec19a3ee90e8d6cceaa5cb9f414
Daniel Cruz <danielcruzp@unitec.edu>
2026-06-14 19:08:40 -0600

    Agregada vista de olvide mi contraseña sin llamado de rutas real

--- src/router/index.ts
+++ src/router/index.ts
@@ -41,6 +41,11 @@ const router = createRouter({
       name: 'admin-sala-create',
       component: () => import('@/views/admin/SalaFormView.vue'),
     },
+    {
+      path: '/forgot-password',
+      name: 'forgot-password',
+      component: () => import('@/views/auth/FotgotPasswordView.vue'),
+    },
   ],
 })
 
